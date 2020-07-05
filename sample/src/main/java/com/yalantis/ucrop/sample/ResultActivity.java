dcc287ada08122a6193d0deaaf9d33c2961ab851
Vaibhav <you@example.com>
2020-07-06 04:35:08 +0530

    Initial commit

---
+++ sample/src/main/java/com/yalantis/ucrop/sample/ResultActivity.java
@@ -0,0 +1,332 @@
+package com.yalantis.ucrop.sample;
+
+import android.Manifest;
+import android.annotation.TargetApi;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.content.pm.ResolveInfo;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Build;
+import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.Toast;
+
+import com.yalantis.ucrop.view.UCropView;
+import com.zomato.photofilters.FilterPack;
+
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.nio.channels.FileChannel;
+import java.util.Calendar;
+import java.util.List;
+
+import androidx.annotation.NonNull;
+import androidx.appcompat.app.ActionBar;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.app.ActivityCompat;
+import androidx.core.app.NotificationCompat;
+import androidx.core.content.ContextCompat;
+import androidx.core.content.FileProvider;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
+
+import ja.burhanrashid52.photoeditor.PhotoEditor;
+import ja.burhanrashid52.photoeditor.PhotoEditorView;
+import ja.burhanrashid52.photoeditor.PhotoFilter;
+import ja.burhanrashid52.photoeditor.SaveSettings;
+
+import static android.content.Intent.FLAG_GRANT_READ_URI_PERMISSION;
+import static android.content.Intent.FLAG_GRANT_WRITE_URI_PERMISSION;
+
+/**
+ * Created by Oleksii Shliama (https://github.com/shliama).
+ */
+public class ResultActivity extends BaseActivity implements FilterListener {
+
+    private static final String TAG = "ResultActivity";
+    private static final String CHANNEL_ID = "3000";
+    private static final int READ_WRITE_STORAGE = 52;
+    private FilterViewAdapter mFilterViewAdapter = new FilterViewAdapter(this);
+    private PhotoEditorView mPhotoEditorView;
+    PhotoEditor mPhotoEditor;
+    private Button saveImage;
+    private RecyclerView mRvFilters;
+    // private PropertiesBSFragment mPropertiesBSFragment;
+    private static final int DOWNLOAD_NOTIFICATION_ID_DONE = 911;
+    private static final int PERMISSION_REQUEST_CODE = 100;
+
+    public static void startWithUri(@NonNull Context context, @NonNull Uri uri) {
+        Intent intent = new Intent(context, ResultActivity.class);
+        intent.setData(uri);
+        context.startActivity(intent);
+    }
+
+    public boolean requestPermission(String permission) {
+        boolean isGranted = ContextCompat.checkSelfPermission(this, permission) == PackageManager.PERMISSION_GRANTED;
+        if (!isGranted) {
+            ActivityCompat.requestPermissions(
+                    this,
+                    new String[]{permission},
+                    READ_WRITE_STORAGE);
+        }
+        return isGranted;
+    }
+
+    public void isPermissionGranted(boolean isGranted, String permission) {
+
+    }
+
+    private boolean checkPermission() {
+        int result = ContextCompat.checkSelfPermission(ResultActivity.this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE);
+        if (result == PackageManager.PERMISSION_GRANTED) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+    private void requestPermission() {
+        if (ActivityCompat.shouldShowRequestPermissionRationale(ResultActivity.this, android.Manifest.permission.WRITE_EXTERNAL_STORAGE)) {
+            Toast.makeText(ResultActivity.this, "Write External Storage permission allows us to save files. Please allow this permission in App Settings.", Toast.LENGTH_LONG).show();
+        } else {
+            ActivityCompat.requestPermissions(ResultActivity.this, new String[]{android.Manifest.permission.WRITE_EXTERNAL_STORAGE}, PERMISSION_REQUEST_CODE);
+        }
+    }
+
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_result);
+        mPhotoEditorView = findViewById(R.id.photoEditorView);
+        saveImage = findViewById(R.id.saveImageFilter);
+        mRvFilters = findViewById(R.id.rvFilterView);
+        LinearLayoutManager llmFilters = new LinearLayoutManager(this, LinearLayoutManager.HORIZONTAL, false);
+        mRvFilters.setLayoutManager(llmFilters);
+        mRvFilters.setAdapter(mFilterViewAdapter);
+
+        mPhotoEditor = new PhotoEditor.Builder(this, mPhotoEditorView)
+                .setPinchTextScalable(false)
+                .build();
+
+        saveImage.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String state = Environment.getExternalStorageState();
+                if (Environment.MEDIA_MOUNTED.equals(state)) {
+                    if (Build.VERSION.SDK_INT >= 23) {
+                        if (checkPermission()) {
+                            String path = Environment.getExternalStorageDirectory().toString();
+                            File file = new File(Environment.getExternalStorageDirectory()
+                                    + File.separator + ""
+                                    + System.currentTimeMillis() + ".png");
+                            if (!file.exists()) {
+                                Log.d("path", file.toString());
+                                try {
+                                    file.createNewFile();
+                                    mPhotoEditor.saveAsFile(file.getAbsolutePath(), new PhotoEditor.OnSaveListener() {
+                                        @Override
+                                        public void onSuccess(@NonNull String imagePath) {
+                                            Toast.makeText(ResultActivity.this, "saved", Toast.LENGTH_SHORT).show();
+                                            Uri mSaveImageUri = Uri.fromFile(new File(imagePath));
+                                            mPhotoEditorView.getSource().setImageURI(mSaveImageUri);
+                                        }
+
+                                        @Override
+                                        public void onFailure(@NonNull Exception exception) {
+
+                                        }
+                                    });
+
+                                } catch (Exception e) {
+                                    e.printStackTrace();
+                                }
+                            }
+                        } else {
+                            requestPermission(); // Code for permission
+                        }
+                    }
+                }
+            }
+        });
+
+
+
+
+
+        Uri uri = getIntent().getData();
+        if (uri != null) {
+            try {
+                Bitmap bitmap = MediaStore.Images.Media.getBitmap(getContentResolver(), uri);
+                mPhotoEditorView.getSource().setImageBitmap(bitmap);
+                UCropView uCropView = findViewById(R.id.ucrop);
+                uCropView.getCropImageView().setImageUri(uri, null);
+                uCropView.getOverlayView().setShowCropFrame(false);
+                uCropView.getOverlayView().setShowCropGrid(false);
+                uCropView.getOverlayView().setDimmedColor(Color.TRANSPARENT);
+            } catch (Exception e) {
+                Log.e(TAG, "setImageUri", e);
+                Toast.makeText(this, e.getMessage(), Toast.LENGTH_LONG).show();
+            }
+        }
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inJustDecodeBounds = true;
+        BitmapFactory.decodeFile(new File(getIntent().getData().getPath()).getAbsolutePath(), options);
+
+        setSupportActionBar((Toolbar) findViewById(R.id.toolbar));
+        final ActionBar actionBar = getSupportActionBar();
+        if (actionBar != null) {
+            actionBar.setDisplayHomeAsUpEnabled(true);
+            actionBar.setTitle(getString(R.string.format_crop_result_d_d, options.outWidth, options.outHeight));
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(final Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_result, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.menu_download) {
+            saveCroppedImage();
+        } else if (item.getItemId() == android.R.id.home) {
+            onBackPressed();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+
+    /**
+     * Callback received when a permissions request has been completed.
+     */
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case REQUEST_STORAGE_WRITE_ACCESS_PERMISSION:
+                if (grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                    saveCroppedImage();
+                }
+                break;
+            default:
+                super.onRequestPermissionsResult(requestCode, permissions, grantResults);
+        }
+    }
+
+    private void saveCroppedImage() {
+        if (ActivityCompat.checkSelfPermission(this, Manifest.permission.WRITE_EXTERNAL_STORAGE)
+                != PackageManager.PERMISSION_GRANTED) {
+            requestPermission(Manifest.permission.WRITE_EXTERNAL_STORAGE,
+                    getString(R.string.permission_write_storage_rationale),
+                    REQUEST_STORAGE_WRITE_ACCESS_PERMISSION);
+        } else {
+            Uri imageUri = getIntent().getData();
+
+            if (imageUri != null && imageUri.getScheme().equals("file")) {
+                try {
+
+                    copyFileToDownloads(getIntent().getData());
+                } catch (Exception e) {
+                    Toast.makeText(ResultActivity.this, e.getMessage(), Toast.LENGTH_SHORT).show();
+                    Log.e(TAG, imageUri.toString(), e);
+                }
+            } else {
+                Toast.makeText(ResultActivity.this, getString(R.string.toast_unexpected_error), Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+    private void copyFileToDownloads(Uri croppedFileUri) throws Exception {
+        String downloadsDirectoryPath = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DOWNLOADS).getAbsolutePath();
+        String filename = String.format("%d_%s", Calendar.getInstance().getTimeInMillis(), croppedFileUri.getLastPathSegment());
+
+        File saveFile = new File(downloadsDirectoryPath, filename);
+
+        FileInputStream inStream = new FileInputStream(new File(croppedFileUri.getPath()));
+        FileOutputStream outStream = new FileOutputStream(saveFile);
+        FileChannel inChannel = inStream.getChannel();
+        FileChannel outChannel = outStream.getChannel();
+        inChannel.transferTo(0, inChannel.size(), outChannel);
+        inStream.close();
+        outStream.close();
+
+        showNotification(saveFile);
+        Toast.makeText(this, R.string.notification_image_saved, Toast.LENGTH_SHORT).show();
+        finish();
+    }
+
+    private void showNotification(@NonNull File file) {
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        intent.addFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        Uri fileUri = FileProvider.getUriForFile(
+                this,
+                getString(R.string.file_provider_authorities),
+                file);
+
+        intent.setDataAndType(fileUri, "image/*");
+
+        List<ResolveInfo> resInfoList = getPackageManager().queryIntentActivities(
+                intent,
+                PackageManager.MATCH_DEFAULT_ONLY);
+        for (ResolveInfo info : resInfoList) {
+            grantUriPermission(
+                    info.activityInfo.packageName,
+                    fileUri, FLAG_GRANT_WRITE_URI_PERMISSION | FLAG_GRANT_READ_URI_PERMISSION);
+        }
+
+        NotificationCompat.Builder notificationBuilder;
+        NotificationManager notificationManager = (NotificationManager) this
+                .getSystemService(Context.NOTIFICATION_SERVICE);
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
+            if (notificationManager != null) {
+                notificationManager.createNotificationChannel(createChannel());
+            }
+            notificationBuilder = new NotificationCompat.Builder(this, CHANNEL_ID);
+        } else {
+            notificationBuilder = new NotificationCompat.Builder(this);
+        }
+
+        notificationBuilder
+                .setContentTitle(getString(R.string.app_name))
+                .setContentText(getString(R.string.notification_image_saved_click_to_preview))
+                .setTicker(getString(R.string.notification_image_saved))
+                .setSmallIcon(R.drawable.ic_done)
+                .setOngoing(false)
+                .setContentIntent(PendingIntent.getActivity(this, 0, intent, 0))
+                .setAutoCancel(true);
+        if (notificationManager != null) {
+            notificationManager.notify(DOWNLOAD_NOTIFICATION_ID_DONE, notificationBuilder.build());
+        }
+    }
+
+    @TargetApi(Build.VERSION_CODES.O)
+    public NotificationChannel createChannel() {
+        int importance = NotificationManager.IMPORTANCE_LOW;
+        NotificationChannel channel = new NotificationChannel(CHANNEL_ID, getString(R.string.channel_name), importance);
+        channel.setDescription(getString(R.string.channel_description));
+        channel.enableLights(true);
+        channel.setLightColor(Color.YELLOW);
+        return channel;
+    }
+
+    @Override
+    public void onFilterSelected(PhotoFilter photoFilter) {
+        mPhotoEditor.setFilterEffect(photoFilter);
+    }
+}
